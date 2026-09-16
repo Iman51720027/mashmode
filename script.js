38746b437b6e3027b798ec99b8fd1b4a94573c78
Bijkar program YT <sikdariman82@gmail.com>
2026-09-16 11:42:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var p = document.getElementsByTagName("p")[0];
 var hr = document.getElementsByTagName("hr")[0];
 var h3= document.getElementsByTagName("h3")[0];
 var a = document.getElementsByTagName("a")[0];
+var backBtn = document.getElementById("back-btn");
 setting_icon.addEventListener('click',function () {
   setting_icon.style.display = "none";
   h1.style.display = "none";
@@ -11,4 +12,15 @@ setting_icon.addEventListener('click',function () {
   hr.style.display = "none";
   h3.style.display = "block";
   a.style.display = "inline";
+  backBtn.style.display = "inline";
+})
+
+backBtn.addEventListener('click',function () {
+  setting_icon.style.display = "block";
+  h1.style.display = "block";
+  p.style.display = "block";
+  hr.style.display = "block";
+  h3.style.display = "none";
+  a.style.display = "none";
+  backBtn.style.display = "none";
 })
